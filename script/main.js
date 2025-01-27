d80af34e7bbb91f09e504bcad762ce7871b9e629
AMMARQf <164189803+AMMARQf@users.noreply.github.com>
2025-01-27 17:16:37 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,8 +1,11 @@
 
 // theme button
 
-const defaultTheme = localStorage.getItem('theme');
+const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
+localStorage.setItem('theme', theme);
+
 const themeBtnMain = document.getElementById('theme-btn-main');
+const defaultTheme = localStorage.getItem('theme');
 
 if (defaultTheme === 'dark') {
     document.body.classList.add('dark-mode');
@@ -17,6 +20,6 @@ themeBtnMain.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     const theme = document.body.classList.contains('dark-mode') ? 'dark' : 'light';
     localStorage.setItem('theme', theme);
-    themeBtnMain.innerHTML = localStorage.getItem('theme')
+    themeBtnMain.innerHTML = localStorage.getItem('theme');
 });
 
